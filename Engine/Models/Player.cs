2019a89475d389b5e62ed34822b5fd941c8390ed
Abdullo <abdullakhzukhriddinov@gmail.com>
2024-02-21 22:38:08 +0500

    Add a Player class and create a model file

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        string Name { get; set; }
+        string CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
+    }
+}
