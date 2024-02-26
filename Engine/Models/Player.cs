0b05c08c5b5c1ae35cfc923b72e6937c29e142bc
Abdullo <abdullakhzukhriddinov@gmail.com>
2024-02-26 16:13:35 +0500

    refactor:Create the Player Inventory

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -70,6 +71,11 @@ namespace Engine.Models
                 _gold = value;
                 OnPropertyChanged(nameof(Gold));
             }
+        }
+        public ObservableCollection<GameItem> Inventory { get; set; }
+        public Player()
+        {
+            Inventory = new ObservableCollection<GameItem>();
         }
 
    
