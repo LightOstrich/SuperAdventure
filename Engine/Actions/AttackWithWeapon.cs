636509f57fbb30f4971514a21657b90767881e26
Abdullo <abdullakhzukhriddinov@gmail.com>
2024-03-13 15:28:35 +0500

    refactor: Add a Base class to action classes

--- Engine/Actions/AttackWithWeapon.cs
+++ Engine/Actions/AttackWithWeapon.cs
@@ -3,17 +3,15 @@ using System;
 
 namespace Engine.Actions
 {
-    public class AttackWithWeapon : IAction
+    public class AttackWithWeapon : BaseAction, IAction
     {
-        private readonly GameItem _weapon;
         private readonly int _maximumDamage;
         private readonly int _minimumDamage;
-        public event EventHandler<string> OnActionPerformed;
-        public AttackWithWeapon(GameItem weapon, int minimumDamage, int maximumDamage)
+        public AttackWithWeapon(GameItem itemInUse, int minimumDamage, int maximumDamage) : base(itemInUse)
         {
-            if (weapon.Category != GameItem.ItemCategory.Weapon)
+            if (itemInUse.Category != GameItem.ItemCategory.Weapon)
             {
-                throw new ArgumentException($"{weapon.Name} is not a weapon");
+                throw new ArgumentException($"{itemInUse.Name} is not a weapon");
             }
             if (_minimumDamage < 0)
             {
@@ -23,7 +21,6 @@ namespace Engine.Actions
             {
                 throw new ArgumentException("maximumDamage must be >= minimumDamage");
             }
-            _weapon = weapon;
             _maximumDamage = maximumDamage;
             _minimumDamage = minimumDamage;
         }
@@ -41,11 +38,6 @@ namespace Engine.Actions
                 ReportResult($"{actorName} hit {targetName} for {damage} point{(damage > 1 ? "s" : "")}.");
                 target.TakeDamage(damage);
             }
-        }
-
-        private void ReportResult(string result)
-        {
-            OnActionPerformed?.Invoke(this, result);
         }
     }
 }
---
+++ Engine/Actions/BaseAction.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Models;
+using System;
+
+namespace Engine.Actions
+{
+    public abstract class BaseAction
+    {
+        protected readonly GameItem _itemInUse;
+        public event EventHandler<string> OnActionPerformed;
+        protected BaseAction(GameItem itemInUse)
+        {
+            _itemInUse = itemInUse;
+        }
+        protected void ReportResult(string result)
+        {
+            OnActionPerformed?.Invoke(this, result);
+        }
+    }
+}
