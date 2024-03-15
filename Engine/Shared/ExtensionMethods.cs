afcfef44ed01ef1c819aca772ca81e5118ecc734
Abdullo <abdullakhzukhriddinov@gmail.com>
2024-03-15 15:17:26 +0500

    feat: Create a Extension methods for XmlNode

---
+++ Engine/Shared/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+using System;
+
+namespace Engine.Shared
+{
+    public static class ExtensionMethods
+    {
+        public static int AttributeAsInt(this XmlNode node, string attributeName)
+        {
+            return Convert.ToInt32(node.AttributeAsString(attributeName));
+        }
+        public static string AttributeAsString(this XmlNode node, string attributeName)
+        {
+            XmlAttribute attribute = node.Attributes?[attributeName];
+            if (attribute == null)
+            {
+                throw new ArgumentException($"The attribute '{attributeName}' does not exist");
+            }
+            return attribute.Value;
+        }
+    }
+}
