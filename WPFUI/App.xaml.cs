209ba9c95161889bd070ce12e66ca753d9f7453f
Abdullo <abdullakhzukhriddinov@gmail.com>
2024-03-16 22:08:31 +0500

    feat: Add a Logging service

--- WPFUI/App.xaml.cs
+++ WPFUI/App.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
+﻿using System.Windows;
+using System.Windows.Threading;
+using Engine.Services;
 namespace WPFUI
 {
     /// <summary>
@@ -13,5 +8,13 @@ namespace WPFUI
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText =
+                $"An exception occurred: {e.Exception.Message}\r\n\r\nat: {e.Exception.StackTrace}";
+            LoggingService.Log(e.Exception);
+            // TODO: Create a Window to display the exception information.
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception", MessageBoxButton.OK);
+        }
     }
 }
