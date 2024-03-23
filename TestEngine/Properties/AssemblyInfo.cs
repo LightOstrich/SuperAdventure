7ce4987f6c30854f49e64c12ee385dcd2f9cb77d
Abdullo <abdullakhzukhriddinov@gmail.com>
2024-03-23 16:14:41 +0500

    feat: Migrate from Dotnet FrameWork to DotNet 8.0

--- TestEngine/Properties/AssemblyInfo.cs
+++ TestEngine/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("TestEngine")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestEngine")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2a809fde-50ca-4c62-94fc-c70703eb340a")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
